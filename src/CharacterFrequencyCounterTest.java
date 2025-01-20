d5f7f2499ed6d2da503daa902f31c67829a250fc
Hunter Y <huntersy99@gmail.com>
2025-01-19 23:46:24 -0800

    implemented JUnit tests

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,20 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+     @Test
+     public void testLAppearsThreeTimesInHelloWorld() {
+          // Arrange
+          CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello world");
+          // Act
+          int actualCount = counter.getFrequency('l');
+          // Assert
+          assertEquals(3, actualCount);
+     }
+
+     @Test
+     public void testNonOccuringCharacterPercentageIsZero() {
+          CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hi there");
+          double actualPercentage = counter.getRelativePercentage('z');
+          assertEquals(0.0, actualPercentage, 0.0001);
+     }
 }
